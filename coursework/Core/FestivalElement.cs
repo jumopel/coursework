abf40ed0b27ed88db9365beefbba34d232817bdd
jumopel <tribalka6.gmail.com>
2026-04-24 22:51:05 +0300

    feat: add Name to FestivalElement

--- coursework/Core/FestivalElement.cs
+++ coursework/Core/FestivalElement.cs
@@ -8,6 +8,7 @@ namespace coursework.Core
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Description { get; set; } = "Опис відсутній";
+        public string Name { get; set; } = "Назва відсутня";
         public abstract string GetReport();
         public abstract double CalculateKPI();
         public abstract string GetKPIDescription();
