88d30fa2200fc1b31c0635eb9b5f5e2ee6b68949
jumopel <tribalka6.gmail.com>
2026-04-26 23:16:21 +0300

    feat: implement data aggregation and KPI calculation in BaseZone

--- coursework/Core/BaseZone.cs
+++ coursework/Core/BaseZone.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 
 namespace coursework.Core
@@ -8,9 +9,17 @@ namespace coursework.Core
     public abstract class BaseZone : FestivalElement
     {
         public ObservableCollection<BaseShop> Shops { get; private set; }
+
         public double Attractiveness { get; set; }
         public string Theme { get; set; } = string.Empty;
-        public int Capacity { get; private set; }
+        public int Capacity { get;  set; }
+
+        public decimal TotalRevenue => Shops.Sum(s => s.Revenue);
+        public decimal TotalNetProfit => Shops.Sum(s => s.NetProfit);
+
+        public int CurrentVisitors => Shops.Sum(s => s.CurrentQueue);
+        public double AverageQueueLength => Shops.Any() ? Shops.Average(s => s.CurrentQueue) : 0;
+
         protected BaseZone()
         {
             Shops = new ObservableCollection<BaseShop>();
@@ -18,16 +27,29 @@ namespace coursework.Core
         public void AddShop(BaseShop shop) =>  Shops.Add(shop);
         public void RemoveShop(BaseShop shop) => Shops.Remove(shop);
 
-        //public double AverageQueueLength()
-        //public decimal CalculateTotalRevenue()
-        //public override double CalculateKPI()
+        public override double CalculateKPI()
+        {
+            if (!Shops.Any()) return 0;
+            double avgShopKPI = Shops.Average(s => s.CalculateKPI());
+            double crowdingPenalty = Capacity > 0 ? (double)CurrentVisitors / Capacity : 0;
+            if (crowdingPenalty > 1) crowdingPenalty = 1;
+            double finalKPI = (avgShopKPI * Attractiveness) * (1 - (crowdingPenalty * 0.5));
+
+            return Math.Round(finalKPI, 2);
+        }
         public override string GetKPIDescription()
         {
-            return "";
+            return "KPI зони базується на середньому KPI її магазинів, " +
+             "помноженому на базову привабливість зони. Показник динамічно знижується " +
+             $"до 50%, якщо кількість відвідувачів перевищує місткість ({Capacity} осіб).";
         }
         public override string GetReport()
         {
-            return "";
+            return $"Зона: '{Name}' [{Theme}]. " +
+                               $"Магазинів: {Shops.Count}. " +
+                               $"Чистий прибуток: {TotalNetProfit} грн. " +
+                               $"Людей у зоні: {CurrentVisitors}/{Capacity} (Сер. черга: {Math.Round(AverageQueueLength, 1)}). " +
+                               $"KPI: {CalculateKPI()}.";
         }
 
 
