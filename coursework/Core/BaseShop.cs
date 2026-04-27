4f180ba447112d175f0466ba6468e74f923f6e38
jumopel <tribalka6.gmail.com>
2026-04-27 23:47:27 +0300

    refactor: implement reactive properties and simulation logic in BaseShop

--- coursework/Core/BaseShop.cs
+++ coursework/Core/BaseShop.cs
@@ -6,27 +6,110 @@ namespace coursework.Core
 {
     public abstract class BaseShop : FestivalElement
     {
-        public double BaseAttractiveness { get; set; } = 1.0;
+        private double _baseAttractiveness = 1.0;
+        private decimal _revenue;
+        private decimal _totalConsumablesCost;
+        private decimal _averageCheck;
+        private decimal _dailyRent;
+        private decimal _staffsDailySalary;
+        private double _marginMultiplier;
 
-        public decimal Revenue { get; protected set; } 
-        public decimal TotalConsumablesCost { get; protected set; }
-        public decimal AverageCheck { get; set; }
-        public decimal DailyRent { get; set; }
-        public decimal StaffsDailySalary { get; set; }
+        private int _cashiersCount;
+        private int _cooksCount;
+        private int _cashierQueue;
+        private int _kitchenQueue;
+
+        private TimeSpan _foodPreparationTime;
+        private TimeSpan _orderTakingTime;
+        private TimeSpan _baseServiceTime;
+
+        private int _totalCustomersServed;
+        private double _totalWaitTimeMinutes;
+
+        public double BaseAttractiveness
+        {
+            get => _baseAttractiveness;
+            set => SetProperty(ref _baseAttractiveness, value);
+        }
+
+        public decimal AverageCheck
+        {
+            get => _averageCheck;
+            set => SetProperty(ref _averageCheck, value);
+        }
+        public decimal DailyRent
+        {
+            get => _dailyRent;
+            set => SetProperty(ref _dailyRent, value);
+        }
+        public decimal StaffsDailySalary
+        {
+            get => _staffsDailySalary;
+            set => SetProperty(ref _staffsDailySalary, value);
+        }
+        public int CashiersCount
+        {
+            get => _cashiersCount;
+            set => SetProperty(ref _cashiersCount, value);
+        }
+        public int CooksCount
+        {
+            get => _cooksCount;
+            set => SetProperty(ref _cooksCount, value);
+        }
+        public TimeSpan FoodPreparationTime
+        {
+            get => _foodPreparationTime;
+            set => SetProperty(ref _foodPreparationTime, value);
+        }
+        public TimeSpan OrderTakingTime
+        {
+            get => _orderTakingTime;
+            set => SetProperty(ref _orderTakingTime, value);
+        }
+        public decimal Revenue
+        {
+            get => _revenue;
+            protected set
+            {
+                if (SetProperty(ref _revenue, value)) OnPropertyChanged(nameof(NetProfit));
+            }
+        }
+        public decimal TotalConsumablesCost
+        {
+            get => _totalConsumablesCost;
+            protected set
+            {
+                if (SetProperty(ref _totalConsumablesCost, value)) OnPropertyChanged(nameof(NetProfit));
+            }
+        }
         public double MarginMultiplier { get; set; }
         public decimal NetProfit => Revenue - TotalConsumablesCost - DailyRent - StaffsDailySalary;
-
-
-        public int CashiersCount { get; set; }
-        public int CooksCount { get; set; }
-
-        public int CashierQueue { get; protected set; }
-        public int KitchenQueue { get; protected set; }
-
+        public int CashierQueue
+        {
+            get => _cashierQueue;
+            protected set
+            {
+                if (SetProperty(ref _cashierQueue, value))
+                {
+                    OnPropertyChanged(nameof(CurrentQueue));
+                    OnPropertyChanged(nameof(CurrentAttractiveness));
+                }
+            }
+        }
+        public int KitchenQueue
+        {
+            get => _kitchenQueue;
+            protected set
+            {
+                if (SetProperty(ref _kitchenQueue, value))
+                {
+                    OnPropertyChanged(nameof(CurrentQueue));
+                    OnPropertyChanged(nameof(CurrentAttractiveness));
+                }
+            }
+        }
         public TimeSpan BaseServiceTime { get; set; }
-        public TimeSpan FoodPreparationTime { get; set; }
-        public TimeSpan OrderTakingTime { get; set; }
-
         public int CurrentQueue => CashierQueue + KitchenQueue;
         public double CurrentAttractiveness
         {
@@ -44,9 +127,24 @@ namespace coursework.Core
                 return actual > 0.3 ? actual : 0.3;
             }
         }
-        public int TotalCustomersServed { get; protected set; } 
-        public double TotalWaitTimeMinutes { get; protected set; }
-        public double AverageServiceSpeedMinutes => TotalCustomersServed > 0 ? TotalWaitTimeMinutes / TotalCustomersServed : 0;
+        public int TotalCustomersServed
+        {
+            get => _totalCustomersServed;
+            protected set
+            {
+                if (SetProperty(ref _totalCustomersServed, value)) OnPropertyChanged(nameof(AverageServiceSpeedMinutes));
+            }
+        }
+        public double TotalWaitTimeMinutes
+        {
+            get => _totalWaitTimeMinutes;
+            protected set
+            {
+                if (SetProperty(ref _totalWaitTimeMinutes, value))
+                    OnPropertyChanged(nameof(AverageServiceSpeedMinutes));
+            }
+        }
+        public double AverageServiceSpeedMinutes => TotalCustomersServed > 0 ? _totalWaitTimeMinutes / TotalCustomersServed : 0;
         protected BaseShop()
         {
             Revenue = 0;
@@ -54,7 +152,7 @@ namespace coursework.Core
             CashierQueue = 0;
             KitchenQueue = 0;
             TotalCustomersServed = 0;
-            TotalWaitTimeMinutes = 0;
+            _totalWaitTimeMinutes = 0;
         }
         public virtual void JoinQueue()
         {
