11117ccc587df58ac1ee4283620034f5ae24d6bd
jumopel <tribalka6.gmail.com>
2026-04-27 22:51:38 +0300

    refactor: make FestivalElement inherit ObservableObject for UI sync

--- coursework/Core/FestivalElement.cs
+++ coursework/Core/FestivalElement.cs
@@ -4,11 +4,21 @@ using System.Text;
 
 namespace coursework.Core
 {
-    public abstract class FestivalElement : ICalculatable
+    public abstract class FestivalElement : ObservableObject, ICalculatable
     {
+        private string _name = string.Empty;
+        private string _description = string.Empty;
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string Description { get; set; } = "Опис відсутній";
-        public string Name { get; set; } = "Назва відсутня";
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value);
+        }
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
         public abstract string GetReport();
         public abstract double CalculateKPI();
         public abstract string GetKPIDescription();
