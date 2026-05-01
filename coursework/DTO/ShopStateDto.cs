7f55e03f59d203aac2efa150cffe6f6de316c03e
jumopel <tribalka6.gmail.com>
2026-05-01 14:18:55 +0300

    feat: add ShopStateDto and ZoneStateDto for data transfer

---
+++ coursework/DTO/ShopStateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coursework.DTO
+{
+    internal class ShopStateDto
+    {
+        public Guid ShopId { get; set; }
+        public string ShopName { get; set; }
+        public decimal CurrentRevenue { get; set; }
+        public int CurrentQueue { get; set; }
+        public double CongestionLevel { get; set; } 
+        public double Attractiveness { get; set; }
+    }
+}
---
+++ coursework/DTO/ZoneStateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coursework.DTO
+{
+    internal class ZoneStateDto
+    {
+        public Guid ZoneId { get; set; }
+        public string ZoneName { get; set; }
+        public string Theme { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public int CurrentVisitors { get; set; }
+        public double OccupancyRate { get; set; } 
+        public List<ShopStateDto> ShopsData { get; set; } = new List<ShopStateDto>();
+    }
+}
+
